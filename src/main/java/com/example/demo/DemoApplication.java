c5ff8fe0eb06a674bb90c9b32ab1708e559580a9
making <makingx@gmail.com>
2017-08-08 16:15:46 +0900

    Add fetch size

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,12 +13,21 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 @EnableAsync
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.setFetchSize(1000);
+		return jdbcTemplate;
 	}
 
 	@Bean
